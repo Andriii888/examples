952fa4f52fd4a5ffb4ae2fe5e5231f4da7e826cb
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2023-01-03 01:56:46 +0300

    delimport

--- src/js/itemContainer.js
+++ src/js/itemContainer.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import Pagination from 'tui-pagination';
-import 'tui-pagination/dist/tui-pagination.css';
+// import 'tui-pagination/dist/tui-pagination.css';
 
 
 const refs = {
